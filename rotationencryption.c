3ee20bdac7e5f19dbf80979bd9515964c8e4b3d8
Simeon Price <simeonaprice@gmail.com>
2019-04-29 01:46:07 +0000

    initial rotation cipher

--- rotationencryption.c
+++ rotationencryption.c
@@ -1,2 +1,37 @@
 #include <stdio.h>
-tgsetgse
+#include <string.h>
+#include <stdlib.h>
+
+void rotationCipher(char* text, int key);
+
+int main(void) {
+    // RANDOM RULES FOR CODE
+    int key = 1;
+    char text[101];
+    
+    printf("Enter the text you want encrypted: ");
+    fgets(text, sizeof(text), stdin);
+    printf("The encrypted text is: ");
+    rotationCipher(text, key);
+    return 0;
+    
+    
+}
+
+void rotationCipher(char* text, int key) {
+    
+    int index = 0;
+    int cipherValue;
+    char cipher;
+    
+    while (text[index] != "\0" && strlen(text) - 1 > index) {
+        
+        cipherValue = (((int)text[index] - 65 + key) % 26 + 65);
+        cipher = (char)(cipherValue);
+        
+        printf("%c", cipher);
+        index++;
+    }
+    printf("\n");
+    
+}
